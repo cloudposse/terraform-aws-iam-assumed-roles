9707fead920baa060e76b94bbd50c3659962579e
aknysh <andriy.knysh@gmail.com>
2018-03-28 00:53:40 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -3,114 +3,131 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "role_trust" {
   statement {
     actions = ["sts:AssumeRole"]
+
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
+
     condition {
       test     = "Bool"
       variable = "aws:MultiFactorAuthPresent"
       values   = ["true"]
     }
-
   }
 }
 
 data "aws_iam_policy_document" "assume_role_admin" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions   = ["sts:AssumeRole"]
     resources = ["${aws_iam_role.admin.arn}"]
   }
 }
+
 resource "aws_iam_policy" "assume_role_admin" {
-  name        = "AssumeRoleAdmin"
-  policy      = "${data.aws_iam_policy_document.assume_role_admin.json}"
+  name   = "AssumeRoleAdmin"
+  policy = "${data.aws_iam_policy_document.assume_role_admin.json}"
 }
 
 data "aws_iam_policy_document" "assume_role_readonly" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions   = ["sts:AssumeRole"]
     resources = ["${aws_iam_role.readonly.arn}"]
   }
 }
+
 resource "aws_iam_policy" "assume_role_readonly" {
-  name        = "AssumeRoleReadOnly"
-  policy      = "${data.aws_iam_policy_document.assume_role_readonly.json}"
+  name   = "AssumeRoleReadOnly"
+  policy = "${data.aws_iam_policy_document.assume_role_readonly.json}"
 }
 
 data "aws_iam_policy_document" "manage_mfa" {
   statement {
     sid = "AllowUsersToCreateEnableResyncDeleteTheirOwnVirtualMFADevice"
+
     actions = [
       "iam:CreateVirtualMFADevice",
       "iam:EnableMFADevice",
       "iam:ResyncMFADevice",
-      "iam:DeleteVirtualMFADevice"
+      "iam:DeleteVirtualMFADevice",
     ]
+
     resources = [
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/&{aws:username}",
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}"
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}",
     ]
   }
+
   statement {
     sid = "AllowUsersToDeactivateTheirOwnVirtualMFADevice"
+
     actions = [
-      "iam:DeactivateMFADevice"
+      "iam:DeactivateMFADevice",
     ]
+
     resources = [
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/&{aws:username}",
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}"
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}",
     ]
+
     condition {
       test     = "Bool"
       variable = "aws:MultiFactorAuthPresent"
       values   = ["true"]
     }
   }
+
   statement {
     sid = "AllowUsersToListMFADevicesandUsersForConsole"
+
     actions = [
       "iam:ListMFADevices",
       "iam:ListVirtualMFADevices",
-      "iam:ListUsers"
+      "iam:ListUsers",
     ]
+
     resources = [
-      "*"
+      "*",
     ]
   }
 }
+
 resource "aws_iam_policy" "manage_mfa" {
-  name        = "AllowUsersToDeactivateTheirOwnVirtualMFADevice"
-  policy      = "${data.aws_iam_policy_document.manage_mfa.json}"
+  name   = "AllowUsersToDeactivateTheirOwnVirtualMFADevice"
+  policy = "${data.aws_iam_policy_document.manage_mfa.json}"
 }
 
 data "aws_iam_policy_document" "allow_change_password" {
   statement {
-    actions = ["iam:ChangePassword"]
+    actions   = ["iam:ChangePassword"]
     resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}"]
   }
+
   statement {
-    actions = ["iam:GetAccountPasswordPolicy"]
+    actions   = ["iam:GetAccountPasswordPolicy"]
     resources = ["*"]
   }
 }
-resource "aws_iam_policy" "allow_change_password" {
-  name        = "AllowChangePassword"
-  policy      = "${data.aws_iam_policy_document.allow_change_password.json}"
-}
 
+resource "aws_iam_policy" "allow_change_password" {
+  name   = "AllowChangePassword"
+  policy = "${data.aws_iam_policy_document.allow_change_password.json}"
+}
 
 resource "aws_iam_group" "admin" {
   name = "${var.admin_group_name}"
 }
+
 resource "aws_iam_group_policy_attachment" "assume_role_admin" {
   group      = "${aws_iam_group.admin.name}"
   policy_arn = "${aws_iam_policy.assume_role_admin.arn}"
 }
+
 resource "aws_iam_group_policy_attachment" "manage_mfa_admin" {
   group      = "${aws_iam_group.admin.name}"
   policy_arn = "${aws_iam_policy.manage_mfa.arn}"
 }
+
 resource "aws_iam_group_policy_attachment" "allow_chage_password_admin" {
   group      = "${aws_iam_group.admin.name}"
   policy_arn = "${aws_iam_policy.allow_change_password.arn}"
@@ -119,33 +136,38 @@ resource "aws_iam_group_policy_attachment" "allow_chage_password_admin" {
 resource "aws_iam_group" "readonly" {
   name = "${var.readonly_group_name}"
 }
+
 resource "aws_iam_group_policy_attachment" "assume_role_readonly" {
   group      = "${aws_iam_group.readonly.name}"
   policy_arn = "${aws_iam_policy.assume_role_readonly.arn}"
 }
+
 resource "aws_iam_group_policy_attachment" "manage_mfa_readonly" {
   group      = "${aws_iam_group.readonly.name}"
   policy_arn = "${aws_iam_policy.manage_mfa.arn}"
 }
+
 resource "aws_iam_group_policy_attachment" "allow_change_password_readonly" {
   group      = "${aws_iam_group.readonly.name}"
   policy_arn = "${aws_iam_policy.allow_change_password.arn}"
 }
 
 resource "aws_iam_role" "admin" {
-  name = "${var.admin_role_name}"
+  name               = "${var.admin_role_name}"
   assume_role_policy = "${data.aws_iam_policy_document.role_trust.json}"
 }
+
 resource "aws_iam_role_policy_attachment" "admin" {
-    role       = "${aws_iam_role.admin.name}"
-    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = "${aws_iam_role.admin.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_role" "readonly" {
-  name = "${var.readonly_role_name}"
+  name               = "${var.readonly_role_name}"
   assume_role_policy = "${data.aws_iam_policy_document.role_trust.json}"
 }
+
 resource "aws_iam_role_policy_attachment" "readonly" {
-    role       = "${aws_iam_role.readonly.name}"
-    policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  role       = "${aws_iam_role.readonly.name}"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,11 @@
 output "group_admin_id" {
   value = "${aws_iam_group.admin.id}"
 }
+
 output "group_admin_arn" {
   value = "${aws_iam_group.admin.arn}"
 }
+
 output "group_admin_name" {
   value = "${aws_iam_group.admin.name}"
 }
@@ -13,9 +15,11 @@ output "group_admin_name" {
 output "group_readonly_id" {
   value = "${aws_iam_group.readonly.id}"
 }
+
 output "group_readonly_arn" {
   value = "${aws_iam_group.readonly.arn}"
 }
+
 output "group_readonly_name" {
   value = "${aws_iam_group.readonly.name}"
 }
@@ -25,6 +29,7 @@ output "group_readonly_name" {
 output "role_admin_arn" {
   value = "${aws_iam_role.admin.arn}"
 }
+
 output "role_admin_name" {
   value = "${aws_iam_role.admin.name}"
 }
@@ -32,7 +37,7 @@ output "role_admin_name" {
 output "role_readonly_arn" {
   value = "${aws_iam_role.readonly.arn}"
 }
+
 output "role_readonly_name" {
   value = "${aws_iam_role.readonly.name}"
 }
-
