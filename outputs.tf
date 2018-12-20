ca4f82662ce45ad0b65235eed674fc723b1b53c1
Josh Myers <joshuajmyers@gmail.com>
2018-12-20 13:51:11 +0000

    Use join/splat on outputs convert list to string
    Otherwise we can have problems in downstream modules and errors are 
    raised:
    
    See https://github.com/cloudposse/terraform-aws-iam-assumed-roles/pull/12 
    for context

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,53 @@
 # Group outputs
 #
 output "group_admin_id" {
-  value       = "${aws_iam_group.admin.id}"
+  value       = "${join("", aws_iam_group.admin.id.*.id)}"
   description = "Admin group ID"
 }
 
 output "group_admin_arn" {
-  value       = "${aws_iam_group.admin.arn}"
+  value       = "${join("", aws_iam_group.admin.*.arn)}"
   description = "Admin group ARN"
 }
 
 output "group_admin_name" {
-  value       = "${aws_iam_group.admin.name}"
+  value       = "${join("", aws_iam_group.admin.*.name)}"
   description = "Admin group name"
 }
 
 output "group_readonly_id" {
-  value       = "${aws_iam_group.readonly.id}"
+  value       = "${join("",  aws_iam_group.readonly.*.id)}"
   description = "Readonly group ID"
 }
 
 output "group_readonly_arn" {
-  value       = "${aws_iam_group.readonly.arn}"
+  value       = "${join("", aws_iam_group.readonly.*.arn)}"
   description = "Readonly group ARN"
 }
 
 output "group_readonly_name" {
-  value       = "${aws_iam_group.readonly.name}"
+  value       = "${join("", aws_iam_group.readonly.*.name)}"
   description = "Readonly group name"
 }
 
 # Role outputs
 #
 output "role_admin_arn" {
-  value       = "${aws_iam_role.admin.arn}"
+  value       = "${join("", aws_iam_role.admin.*.arn)}"
   description = "Admin role ARN"
 }
 
 output "role_admin_name" {
-  value       = "${aws_iam_role.admin.name}"
+  value       = "${join("", aws_iam_role.admin.*.name)}"
   description = "Admin role name"
 }
 
 output "role_readonly_arn" {
-  value       = "${aws_iam_role.readonly.arn}"
+  value       = "${join("", aws_iam_role.readonly.*.arn)}"
   description = "Readonly role ARN"
 }
 
 output "role_readonly_name" {
-  value       = "${aws_iam_role.readonly.name}"
+  value       = "${join("", aws_iam_role.readonly.*.name)}"
   description = "Readonly role name"
 }
