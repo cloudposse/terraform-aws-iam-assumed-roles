2eea8ec355ab89a71a7e82a6073726185a2168a9
milo0 <milo0@posteo.de>
2019-08-11 11:52:40 +0200

    Update HCL to tf12 syntax

--- variables.tf
+++ variables.tf
@@ -1,63 +1,64 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = string
   description = "Set to false to prevent the module from creating any resources"
   default     = "true"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "admin_name" {
-  type        = "string"
+  type        = string
   default     = "admin"
   description = "Name for the admin group and role (e.g. `admin`)"
 }
 
 variable "readonly_name" {
-  type        = "string"
+  type        = string
   default     = "readonly"
   description = "Name for the readonly group and role (e.g. `readonly`)"
 }
 
 variable "admin_user_names" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Optional list of IAM user names to add to the admin group"
 }
 
 variable "readonly_user_names" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Optional list of IAM user names to add to the readonly group"
 }
 
 variable "switchrole_url" {
-  type        = "string"
+  type        = string
   description = "URL to the IAM console to switch to a role"
   default     = "https://signin.aws.amazon.com/switchrole?account=%s&roleName=%s&displayName=%s"
 }
+
